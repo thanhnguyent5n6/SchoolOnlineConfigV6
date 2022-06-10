f5f984a694f17e217db22cf33bfc29ea3d05207b
NguyenNT <nguyennt@omt.vn>
2022-06-10 10:02:49 +0700

    parse token

--- src/helpers.php
+++ src/helpers.php
@@ -2,9 +2,28 @@
 
 class SchoolOnlineConfig
 {
-    public static function world()
+    /**
+     * @param String $token
+     * @return array
+     */
+    public function parseTokenJWT(String $token)
     {
-        return 'Hello World, Composer!';
+        try {
+            $tokenParts = explode(".", $token);
+            $tokenHeader = base64_decode($tokenParts[0]);
+            $tokenPayload = base64_decode($tokenParts[1]);
+            $jwtHeader = (array) json_decode($tokenHeader);
+            $jwtPayload = (array) json_decode($tokenPayload);
+            return array(
+                'header' => $jwtHeader,
+                'payload' => $jwtPayload,
+            );
+        } catch (Exception $exception) {
+            return array(
+                'header' => [],
+                'payload' => [],
+            );
+        }
     }
 }
 
