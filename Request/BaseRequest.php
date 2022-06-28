ec20d49b2057dcb38e56226208079424a9272851
NguyenNT <nguyennt@omt.vn>
2022-06-28 17:08:05 +0700

    config

---
+++ Request/BaseRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SchoolOnline\Request;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class BaseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => false,
+            'code'  => Response::HTTP_UNPROCESSABLE_ENTITY,
+            'messages' => $validator->errors()
+        ]));
+    }
+}
