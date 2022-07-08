1351f845e20d15c7d444e09a6b8f8a55d4554999
NguyenNT <nguyennt@omt.vn>
2022-07-08 17:18:31 +0700

    config

--- src/FirebaseReference.php
+++ src/FirebaseReference.php
@@ -4,8 +4,34 @@
 namespace SchoolOnline\Config;
 
 
-class FirebaseReference
+use Carbon\Carbon;
+
+class Firebase
 {
-    const CREATE_MENU_ITEM = 'setting_manager/menu_item/create';
-    const UPDATE_MENU_ITEM = 'setting_manager/menu_item/update';
+    const CREATE_MENU_ITEM = 'menu-item/create';
+    const UPDATE_MENU_ITEM = 'menu-item/update';
+
+    /**
+     * Only using via request
+     *
+     * @param string $userId
+     * @param string $path , from FirebaseReference
+     * @param string $message
+     * @param bool $status
+     * @return bool
+     */
+    public static function addFirebase(string $userId, string $path, string $message, bool $status)
+    {
+        $firebase = app('firebase.database');
+        if (empty($firebase))
+            return false;
+        $path .= "/" . md5($userId);
+        $data = json_encode([
+            'status'  => $status,
+            'message' => $message,
+            'time'    => Carbon::now()->timestamp
+        ]);
+        $firebase->getReference($path)->push($data);
+        return true;
+    }
 }
