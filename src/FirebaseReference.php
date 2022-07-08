2ff38bb5952cc44b87fb45b0dc867d7998dfed1f
NguyenNT <nguyennt@omt.vn>
2022-07-08 16:12:52 +0700

    config

---
+++ src/FirebaseReference.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace SchoolOnline\Config;
+
+
+class FirebaseReference
+{
+    const CREATE_MENU_ITEM = 'setting_manager/menu_item/create';
+    const UPDATE_MENU_ITEM = 'setting_manager/menu_item/update';
+}
