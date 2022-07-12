346d698124838904e1e538f7e84092250c2b5b95
NguyenNT <nguyennt@omt.vn>
2022-07-12 16:08:42 +0700

    config

--- src/Permission.php
+++ src/Permission.php
@@ -24,7 +24,7 @@ class Permission
 
     public static function response403()
     {
-        return response(trans('common.permission.access_denied'), 403);
+        return response(trans('Bạn không có quyền truy cập tính năng này'), 403);
     }
 
     const OMT_ACCESS = "omt_access";
